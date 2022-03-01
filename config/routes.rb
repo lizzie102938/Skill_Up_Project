df0e33949dfcde6ec86622a301ae65789ed1fc86
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-03-01 17:24:56 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :skills, only: [:index, :show] do
+  resources :skills, only: [:index, :show]
+  resources :user_skills, only: [:show ] do
     resources :bookings, only: [:new, :show, :update, :create] do
       resources :reviews, only: [:create]
     end
