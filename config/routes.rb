701ae50f9d4d93adcaa988b6575b3aaba240ed7a
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-03-01 16:56:16 +0100

    added routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :skills, only: [:index, :show] do
+    resources :bookings, only: [:new, :show, :update, :create] do
+      resources :reviews, only: [:create]
+    end
+  end
+  resources :chatrooms, only: [:show] do
+    resources :messages, only: [:create]
+  end
+  get "dashboard", to: "pages#dashboard"
 end
