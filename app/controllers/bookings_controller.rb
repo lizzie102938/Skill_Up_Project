3e12bb06fdc050de150f7f8f80233a2d312c5360
David Morio <david.morio.lesavourey@gmail.com>
2022-03-02 17:50:10 +0100

    Migration for adding a user skill id to booking done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,17 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :create
+
+
+  def create
+    @booking = Booking.new(booking_params)
+    raise
+    @booking.save
+    authorize @booking
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:remote, :message, :student_id, :teacher_id, :date, :user_skill_id)
+  end
 end
