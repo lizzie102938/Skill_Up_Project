44ea76df95dceabe8bb50f268ed21ef4bb6805db
David Morio <david.morio.lesavourey@gmail.com>
2022-03-02 14:16:14 +0100

    Before merging

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,11 +1,13 @@
 class SkillsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @skills = Skill.all
   end
 
   def show
-    @users = User.where()
+    @skill = Skill.find(params[:id])
+    @users = User.all
+    @users = User.where(@users.user_skills.name == @skill.name)
   end
 end
