b81ef6b15c1909edffe16596379a0f601d2aacfd
David Morio <david.morio.lesavourey@gmail.com>
2022-03-02 18:18:43 +0100

    Create booking in progress

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,12 @@
 class BookingsController < ApplicationController
   skip_before_action :authenticate_user!, only: :create
 
-
   def create
+    @user_skill = UserSkill.find(params[:user_skill_id])
+    @skill = @user_skill.skill.name
     @booking = Booking.new(booking_params)
+    @booking.user_skill_id = @user_skill.id
+    @booking.skill = @skill
     raise
     @booking.save
     authorize @booking
