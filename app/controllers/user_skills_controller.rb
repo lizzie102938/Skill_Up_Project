c9db556137962694b1558646e4208ddaac0d24af
David Morio <david.morio.lesavourey@gmail.com>
2022-03-02 17:03:11 +0100

    User skills controller added. Show action created

---
+++ app/controllers/user_skills_controller.rb
@@ -0,0 +1,7 @@
+class UserSkillsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :skills, only: [:index, :show]
-  resources :user_skills do
+  resources :user_skills, only: [:show] do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:show, :update, :destroy] do
