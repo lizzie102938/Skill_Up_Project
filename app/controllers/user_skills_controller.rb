4b8353b4fb5cd612466c6269db4a05670dfda829
David Morio <david.morio.lesavourey@gmail.com>
2022-03-02 17:14:36 +0100

    User skill show implemented. No Css in the view

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -2,6 +2,7 @@ class UserSkillsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
 
   def show
-
+    @user_skill = UserSkill.find(params[:id])
+    authorize @user_skill
   end
 end
