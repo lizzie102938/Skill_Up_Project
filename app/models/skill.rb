46f2ffc1747bbb01b42435e793491366ad21aff7
lizzie lerwill <lizzielerwill@hotmail.com>
2022-03-03 16:56:32 +0100

    homepage done

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,4 +2,10 @@ class Skill < ApplicationRecord
   has_many :user_skills
   has_many :users, through: :user_skills
   validates :name, uniqueness: true
+
+  pg_search_scope :search_by_skill_and_city,
+    against: [ :skill, :city ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
