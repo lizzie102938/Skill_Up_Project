4bc210f7f6ed47c97db40dea5afd1df7bd6e6477
David Morio <david.morio.lesavourey@gmail.com>
2022-03-03 10:38:40 +0100

    Create function done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :create
+  skip_before_action :authenticate_user!
 
   def create
     @user_skill = UserSkill.find(params[:user_skill_id])
@@ -7,10 +7,15 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.user_skill_id = @user_skill.id
     @booking.skill = @skill
-  
-    raise
-    @booking.save
+    @booking.status = 'pending'
+    @booking.student_id = current_user.id
+    @booking.teacher_id = @user_skill.id
     authorize @booking
+    if @booking.save!
+      redirect_to dashboard_path
+    else
+      render 'user_skills/show'
+    end
   end
 
   private
