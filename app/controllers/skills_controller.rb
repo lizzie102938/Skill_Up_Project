b8b000a2d51a386ce1ae432c78dbb6568417367e
lizzie lerwill <lizzielerwill@hotmail.com>
2022-03-07 12:29:02 +0100

    Search and back button

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -7,11 +7,20 @@ class SkillsController < ApplicationController
     else
       @skills = policy_scope(Skill)
     end
+
+    if params[:city].present?
+      @skills = @skills.select { |skill| skill.users.any? { |user| user.location.downcase == params[:city].downcase } }
+      # also need to add case insensitive
+    else
+      @skills
+    end
+
   end
 
   def show
     @skill = Skill.find(params[:id])
     authorize @skill
     @users = policy_scope(User).joins(:user_skills).where(user_skills: { skill: @skill })
+    @users = @users.select { |user| user.location == params[:city] }
   end
 end
