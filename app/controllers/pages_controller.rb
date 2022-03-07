424a2e3f5524a6690fa9d04b1a67b5040f21e9a7
Dorothea von Randow <thearandow@gmail.com>
2022-03-07 16:31:47 +0100

    added skip before action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :how_it_works ]
 
   def home
     if params[:query].present?
