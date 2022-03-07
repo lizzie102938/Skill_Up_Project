1d67fad4a56afdda24f4e5aec421541de90b39b5
lizzie lerwill <lizzielerwill@hotmail.com>
2022-03-07 22:16:28 +0100

    seed issue and map

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -19,9 +19,8 @@ export default class extends Controller {
     mapboxgl.accessToken = this.apiKeyValue
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/lizzie102938/cl0gww57w001214qkldinyi55"
+      style: "mapbox://styles/mapbox/streets-v10"
     })
-
 
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
