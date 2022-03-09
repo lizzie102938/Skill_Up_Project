164cef949456c51df8e0e7b7af56cc594b787887
David Morio <david.morio.lesavourey@gmail.com>
2022-03-09 12:14:46 +0100

    Bookings and requests sorted. We can delete, edit, chat

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,6 @@ class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   belongs_to :student, class_name: "User"
   belongs_to :teacher, class_name: "User"
+
+  # validates :student_id, uniqueness: { scope: [:teacher_id] }
 end
