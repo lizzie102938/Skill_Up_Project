8519165eca642646ef728d0376b4fc610b554d16
David Morio <david.morio.lesavourey@gmail.com>
2022-03-09 16:56:36 +0100

    Booking and request are now visible in the chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,7 +4,7 @@ class ChatroomsController < ApplicationController
     @message = Message.new
     authorize @chatroom
     authorize @message
-    @bookings = Booking.where(student: current_user, teacher: @chatroom.teacher).or(Booking.where(teacher: current_user, student: @chatroom.student))
+    @bookings = Booking.where(student: @chatroom.student, teacher: @chatroom.teacher).or(Booking.where(teacher: @chatroom.student, student: @chatroom.teacher))
   end
 
 end
