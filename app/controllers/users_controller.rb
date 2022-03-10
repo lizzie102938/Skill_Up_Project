fd4643ed035b883a672b73f6ce4c744966c44ebb
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-03-10 18:17:36 +0100

    coin on left side

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ class UsersController < ApplicationController
   end
 
   def update
-    
+    @user_description = current_user
+    @user.description = params[:description] || params[:user][:description]
+    @user_skill.save
+    authorize @user.description
+    if @user_description.save!
+      redirect_to dashboard_path
+    else
+      render
+    end
   end
 end
