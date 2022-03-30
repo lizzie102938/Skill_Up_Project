77fd0886a1366459f6b1b95ef326d7c4df362e12
David Morio <david.morio.lesavourey@gmail.com>
2022-03-30 17:35:34 +0200

    Show and index test done

--- test/controllers/skills_controller_test.rb
+++ test/controllers/skills_controller_test.rb
@@ -7,11 +7,14 @@ class SkillsControllerTest < ActionDispatch::IntegrationTest
 
   test "should get index" do
     get skills_url
+    assert_equal 'cooking', @skill.name
+    assert_select 'h2', 'Your results'
     assert_response :success
   end
 
   test "should show skill" do
     get skill_url(@skill)
+    assert_equal 'cooking', @skill.name
     assert_response :success
   end
 end
