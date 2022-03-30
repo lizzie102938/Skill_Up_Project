778999907b855b272a82205e012bf73d0bbae4c6
David Morio <david.morio.lesavourey@gmail.com>
2022-03-30 14:27:10 +0200

    skills show test in progress

--- test/controllers/skills_controller_test.rb
+++ test/controllers/skills_controller_test.rb
@@ -1,17 +1,18 @@
 require "test_helper"
 
 class SkillsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @skill = skills(:one)
-  end
+  # setup do
+  #   @skill = skills(:one)
+  # end
 
   test "should get index" do
     get skills_url
     assert_response :success
   end
 
-  test "should show skill" do
-    get skill_url(@skill)
-    assert_response :success
-  end
+  # test "should show skill" do
+  #   skill = skills(:one)
+  #   get skill_url(skill)
+  #   assert_response :success
+  # end
 end
