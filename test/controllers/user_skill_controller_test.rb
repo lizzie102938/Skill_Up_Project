a925cac287d24f920bb93f13631e1826f6d0010f
David Morio <david.morio.lesavourey@gmail.com>
2022-03-31 15:03:19 +0200

    User skill test initiated

--- test/controllers/user_skill_controller_test.rb
+++ test/controllers/user_skill_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UserSkillControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_skill = user_skills(:katie_cooking)
+  end
+
+  test "should show a user skill" do
+    # sign_in_as users(:janosch)
+
+    get user_skill_url(@user_skill)
+    assert_response :success
+  end
+
 end
